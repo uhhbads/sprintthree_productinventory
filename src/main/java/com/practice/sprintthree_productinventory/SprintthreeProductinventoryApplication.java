22974930afa66ba21e78c9407085d7b2fddf6bdd
uhhbads <abadshoodee@gmail.com>
2026-02-01 22:14:12 +0800

    init

---
+++ src/main/java/com/practice/sprintthree_productinventory/SprintthreeProductinventoryApplication.java
@@ -0,0 +1,13 @@
+package com.practice.sprintthree_productinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintthreeProductinventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintthreeProductinventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/sprintthree_productinventory/SprintthreeProductinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.sprintthree_productinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintthreeProductinventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
