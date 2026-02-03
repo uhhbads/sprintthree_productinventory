ca4773f0e75703b0602377e6c235618fa84a45bb
uhhbads <abadshoodee@gmail.com>
2026-02-03 17:31:55 +0800

    implemented getAllProducts in controller

--- src/main/java/com/practice/sprintthree_productinventory/controller/ProductController.java
+++ src/main/java/com/practice/sprintthree_productinventory/controller/ProductController.java
@@ -8,12 +8,10 @@ import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -36,6 +34,22 @@ public class ProductController {
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
+                .body(apiResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse> getAllProducts(
+            @RequestParam(required = false) String category){
+        List<ProductResponse> productResponse = productService.getAllProducts(category);
+
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setSuccess(true);
+        apiResponse.setMessage("All products obtained successfully");
+        apiResponse.setData(productResponse);
+        apiResponse.setTimestamp(LocalDateTime.now());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
                 .body(apiResponse);
     }
 }
